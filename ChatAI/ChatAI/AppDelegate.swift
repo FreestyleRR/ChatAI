779c43598b8b67c3471832cfa2eb6eb3fd5b0bf9
Pavel Sharkov <pavlo.sharkov@onix-system.com>
2023-01-10 21:32:43 +0200

    Initial commit

---
+++ ChatAI/ChatAI/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  ChatAI
+//
+//  Created by Pavel Sharkov on 10.01.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ ChatAI/ChatAI/ViewController/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ChatAI
+//
+//  Created by Pavel Sharkov on 10.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+}
+
